ee6531d434eb3965635de493622fcd360c9e7bae
mpcrockett <mpcrockett+github@gmail.com>
2025-03-10 17:12:37 -0400

    basic middleware set up

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest} from 'next/server';
+
+export function middleware(req: NextRequest) {
+  return NextResponse.json(
+    { message: 'Authorization Denied' },
+    { status: 401}
+  )
+};
+
