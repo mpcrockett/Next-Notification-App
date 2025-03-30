37225ffb435ee11f8e6c766afae3cb31421f3f05
mpcrockett <mpcrockett+github@gmail.com>
2025-03-30 17:26:51 -0400

    login/logout and middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+'use client';
 import NotificationForm from "./components/notificationForm";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { status, data: session } = useSession();
+
 
   return (
     <>
-      <h3>Login with <a href="/api/auth/signin">Google</a></h3>
+      {status === 'loading' && <h3>Loading...</h3>}
+      {status === 'authenticated' ? 
+        ( <>
+            <h3>Welcome, {session.user.name}</h3>
+            <h3><a href="/api/auth/signout">Sign Out</a></h3>
+          </>
+        )
+        : <h3>Login with <a href="/api/auth/signin">Google</a></h3>}
       <NotificationForm />
     </>
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,7 @@
-import { NextResponse } from 'next/server'
+export { default } from "next-auth/middleware";
 
-export function middleware() {
-  // return NextResponse.json(
-  //   { message: 'Authorization Denied' },
-  //   { status: 401}
-  // )
-  console.log("middleware");
-  return NextResponse.next();
+export const config = {
+  matcher: ['/api/:path((?!auth$).*)']
 };
+  
 
